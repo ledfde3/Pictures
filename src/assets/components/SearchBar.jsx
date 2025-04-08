119f8e434895ab0c917952564ef6e4c2825e4552
ledfde3 <ayledmafer2807@gmail.com>
2025-04-07 19:47:29 -0700

    Agrege SearchBar.jsx

--- src/assets/components/SearchBar.jsx
+++ src/assets/components/SearchBar.jsx
@@ -1,8 +1,28 @@
+import {useState} from "react"
+import './SearchBar.css'
 
 
-const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
+const SearchBar = ({onSubmit}) => {
+  
+    const [term, setTerm]=useState('')
+
+    const handleFormSubtmit=(event)=>{
+        event.preventDefault()
+        console.log('Necesito decirle al componente papa sobre los datos')
+        onSubmit(term)
+    }
+    const handleChange =(event)=>{
+        setTerm(event.target.value)
+    }
+    console.log('term', term)
+    return ( 
+        <div className="search-bar">
+            <form onSubmit={handleFormSubmit}>
+       <label>Termino de la busqueda:</label>
+       <input onChange={handleChange} value={term}/>
+            </form>
+        </div>
+    
   )
 }
 
