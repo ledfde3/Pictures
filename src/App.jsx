c7dfc3125b935cfc1c22fab9818a029f22ee1bf3
ledfde3 <ayledmafer2807@gmail.com>
2025-04-08 18:29:07 -0700

    Correcion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import SearchBar from "./components/SearchBar"
 import ImageList from "./components/ImageList"
 import searchImages from "./api"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  searchImages('cars')
   return (
     <>
-      <h1>Pictures Apps</h1>
-      <SearchBar/>
-      <ImageList/>
+      <h1>Pictures App</h1>
+      <SearchBar />
+      <ImageList />
     </>
   )
 }
