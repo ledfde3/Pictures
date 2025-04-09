5b01869af5fdbbd98b7bdae57d2220ae07fa0c24
ledfde3 <ayledmafer2807@gmail.com>
2025-04-08 18:59:02 -0700

    Agregado a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import {useState} from "react"
 import SearchBar from "./components/SearchBar"
 import ImageList from "./components/ImageList"
 import searchImages from "./Api"
 
 
 function App() {
-  searchImages('cars')
+  //searchImages('cars')
+  const [images, setImages] = useState ([])
+
+  const handleSubmit  = async (term) => {
+    console.log ('Ustedesta buscando cars ', term)
+    const result = await searchImages (term)
+  }
   return (
     <>
       <h1>Pictures App</h1>
