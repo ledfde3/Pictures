b334f4afd9cee8c2adbd474debcb4779aa8602a5
ledfde3 <ayledmafer2807@gmail.com>
2025-04-08 18:41:39 -0700

    correcion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import SearchBar from "./components/SearchBar"
 import ImageList from "./components/ImageList"
-import searchImages from "./api"
+import searchImages from "./Api"
 
 
 function App() {
